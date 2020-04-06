b01edb4c23cc75bc5f3ac3be408a7a722d806b79
Mikias Ephrem <mikiasephrem@camara.org>
2020-04-06 22:43:53 +0300

    added Account app

--- camara_mis/settings.py
+++ camara_mis/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'camara_mis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'camaramis',
+        'USER': 'camarauser',
+        'PASSWORD': 'Camara2020!',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
